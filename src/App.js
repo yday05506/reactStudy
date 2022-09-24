8939841c4477dea2bd5f8ae14552ce79882029e9
yday <yangdayeon2@gmail.com>
2022-09-24 10:22:09 +0900

    첫 커밋

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Zena";
+  const element = <h1>My name is {name}!!</h1>
+  const element2 = <h1>1 + 1 = {1+1}</h1>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, World!</h1>
+      {element} 
     </div>
   );
 }
