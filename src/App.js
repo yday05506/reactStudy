3e2e991836e2bbc71ff8a78661a65bd3a35d660d
yday <yangdayeon2@gmail.com>
2022-10-29 13:10:33 +0900

    카드

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import {ReactComponent as Good} from './feeling-good.svg'
+import {ReactComponent as Nice} from './feeling-nice.svg'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <div className='card'>
+        <div className='profile-image-1'><Good/></div>
+        <div className='profile-image-2'><Nice/></div>
+        <div className='profile-image-3'><Nice/></div>
+        <div className='question-info'>
+          To <strong>Mr.Good, </strong><br/>
+          From <strong>Ms.Nice</strong>
+        </div>
+        <div className='question'>
+          What's contents 📚🎮🎬 are you enjoying these days?
+        </div>
+        <div className='answer'>
+          <strong>Ms.Nice answered </strong><br/>
+          I am playing Animal Crossing New Horizon.
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
